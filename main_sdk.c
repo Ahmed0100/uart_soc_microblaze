29814003f9c04c3f0a3590494042a186825a4fbb
Ahmed Mustafa <ahmedmustafa0100@gmail.com>
2022-05-12 09:31:38 +0200

    Update main_sdk.c

--- main_sdk.c
+++ main_sdk.c
@@ -8,23 +8,29 @@ int main()
 	u32 receivedBytesNum=0;
 	u32 totalReceivedBytesNum=0;
 	u32 status=0;
-	u32 fileSize = 8;
+	u32 fileSize = 16;
 	XUartLite_Config* myUartConfig= XUartLite_LookupConfig(XPAR_AXI_UARTLITE_0_DEVICE_ID);
 	XUartLite myUart;
 	status = XUartLite_CfgInitialize(&myUart,myUartConfig, myUartConfig->RegBaseAddr);
 	if(status != XST_SUCCESS)
 		xil_printf("Uart init failed\n");
-
+	//read data from the UART interface
 	while(totalReceivedBytesNum < fileSize)
 	{
-		receivedBytesNum = XUartLite_Recv(&myUart,imageData+(sizeof(u32)*totalReceivedBytesNum),8);
+		receivedBytesNum = XUartLite_Recv(&myUart,imageData+(sizeof(u8)*totalReceivedBytesNum),fileSize);
 		totalReceivedBytesNum += receivedBytesNum;
 	}
-
+	//bytes inversion
+	for(int i=0;i<16;i++)
+	{
+		XIo_Out8(imageData, 255-XIo_In8(imageData));
+		imageData+=1;
+	}
+	//write back to stdio through the UART interface
 	for(int i=0;i<fileSize;i++)
 	{
-		xil_printf("d[%d] = %0X\n",i,XIo_In32(imageData));
-		imageData+=4;
+		xil_printf("d[%d] = %0X\n",i,XIo_In8(imageData));
+		imageData+=1;
 	}
 
 }
